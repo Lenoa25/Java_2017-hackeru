49490fdb774aecf10878ec4cfd40acb485d9259e
Lenoa25 <noalerer@gmail.com>
2017-02-12 15:10:49 +0200

    initCommit

---
+++ lesson06-nested classes/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+     //   Shape.color color=new Shape.color(4,4,5);
+
+    }
+}
